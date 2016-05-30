95c978f07aaaa012bc85c57ffaf222d4d764542b
Amos Ang Yong Jian <amosang.yj@gmail.com>
2016-05-30 21:40:42 +1000

    Added caching for text

--- app/src/main/java/com/newsfeed/amosang/newsfeed/MainActivity.java
+++ app/src/main/java/com/newsfeed/amosang/newsfeed/MainActivity.java
@@ -3,6 +3,8 @@ package com.newsfeed.amosang.newsfeed;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -31,6 +34,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -39,14 +44,13 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     private String jsonURL = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=25&q=http://www.abc.net.au/news/feed/51120/rss.xml";
 
     private ArrayList<NewsObj>newsObjs = new ArrayList<>();
-    //show response
-    private String jsonResponse;
     private NewsAdapter na;
     private ListView lv;
     private ProgressDialog progressDialog;
@@ -56,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         if(getSupportActionBar()!=null){
@@ -70,7 +76,32 @@ public class MainActivity extends AppCompatActivity {
         na = new NewsAdapter(new ArrayList<NewsObj>(),this);
         lv = (ListView)findViewById(R.id.lv_newslist);
         lv.setAdapter(na);
-        new AsyncLoadData().execute();
+        //check if there is internet
+        if(checkConnection()==true){
+            String directory = getFilesDir().getAbsolutePath();
+            String filename = "cache";
+            fileDelete(directory,filename);
+            new AsyncLoadData().execute();
+        }else{
+            try{
+                BufferedReader inputReader = new BufferedReader(new InputStreamReader(openFileInput("cache")));
+                String in=null;
+                while((in=inputReader.readLine())!=null){
+                    String[] split = in.split(Pattern.quote("|"));
+                    String title = split[0];
+                    String thumburl = split[1];
+                    String url = split[2];
+                    String publishedDate = split[3];
+                    NewsObj aObj = new NewsObj(title,thumburl,url,publishedDate);
+                    newsObjs.add(aObj);
+                }
+                na.setNews(newsObjs);
+                na.notifyDataSetChanged();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
 
         refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,9 +115,9 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String articleURL = newsObjs.get(position).getUrl();
                 Bundle data = new Bundle();
-                data.putString("url",articleURL);
+                data.putString("url", articleURL);
                 Intent webPage = new Intent();
-                webPage.setClass(getApplicationContext(),WebPage.class);
+                webPage.setClass(getApplicationContext(), WebPage.class);
                 webPage.putExtras(data);
                 startActivity(webPage);
 //                Toast.makeText(MainActivity.this, articleURL, Toast.LENGTH_SHORT).show();
@@ -133,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
             HttpResponse httpResponse = httpClient.execute(httpGet);
             StatusLine statusLine = httpResponse.getStatusLine();
             int statusCode = statusLine.getStatusCode();
-            Log.d("RECEIVEJSONCODE",String.valueOf(statusCode));
+            //Log.d("RECEIVEJSONCODE",String.valueOf(statusCode));
             if(statusCode==200){
                 HttpEntity httpEntity = httpResponse.getEntity();
                 InputStream inputStream = httpEntity.getContent();
@@ -192,7 +223,7 @@ public class MainActivity extends AppCompatActivity {
                 //Log.d("RECEIVEJSONOBJECTLEVEL4", mediaGroups.toString());
 //                main information for news article
 
-//                //for further thumbnail sizes?
+//                //for further thumbnail sizes
                 JSONArray jsonArray2 = new JSONArray();
                 if(mediaItems!=null){
                     jsonArray2 = new JSONArray(mediaItems);
@@ -206,7 +237,6 @@ public class MainActivity extends AppCompatActivity {
                     JSONArray jsonArray3 = new JSONArray(contentItems);
                     for(int k=0;k<jsonArray3.length();k++){
                         JSONObject items = jsonArray3.getJSONObject(k);
-                        //too specific
                         String imgurl = items.getString("url");
                         //Log.d("RECEIVEJSONOBJECTARRAY3",imgurl);
                         String thumbnails = items.getString("thumbnails");
@@ -214,13 +244,10 @@ public class MainActivity extends AppCompatActivity {
                             JSONArray jsonArray4 = new JSONArray(thumbnails);
                             for(int l=0;l<jsonArray4.length();l++){
                                 JSONObject thumbnails1 = jsonArray4.getJSONObject(l);
-
-                                String height = items.getString("height");
-                                String width = items.getString("width");
                                 String thumburl = items.getString("url");
-//
 //                                Log.d("RECEIVEJSONOBJECTTB", thumburl);
                                 NewsObj newsObj = new NewsObj(title, thumburl,url, publishedDate);
+                                cache(newsObj);
                                 newsObjs.add(newsObj);
 
                             }
@@ -234,6 +261,42 @@ public class MainActivity extends AppCompatActivity {
             Log.d("RECEIVEJSONERROR",e.toString());
         }
         return newsObjs;
+    }
+
+    private Boolean checkConnection(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            return true;
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Newsfeed requires internet access in order to function",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    private void cache(NewsObj aObj){
+        String title = aObj.getTitle();
+        String thumburl = aObj.getImgurl();
+        String url = aObj.getUrl();
+        String publishedDate = aObj.getDate();
+        String combined = title +"|"+thumburl+"|"+url+"|"+publishedDate+"\n";
+        try{
+            FileOutputStream fileOutputStream = openFileOutput("cache",MODE_APPEND);
+            fileOutputStream.write(combined.getBytes());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void fileDelete(String directory,String filename){
+        File file = new File(directory,filename);
+        if(file.exists()){
+            file.delete();
+        }else{
+            Log.d("FDEMA", "File Doesn't Exist");
+        }
     }
 
     @Override
