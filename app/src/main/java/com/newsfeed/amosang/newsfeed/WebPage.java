9f2a26e42d453095a8b6130c4fd72286f8c9e898
Amos Ang Yong Jian <amosang.yj@gmail.com>
2016-05-30 19:47:19 +1000

    updated UI

--- app/src/main/java/com/newsfeed/amosang/newsfeed/WebPage.java
+++ app/src/main/java/com/newsfeed/amosang/newsfeed/WebPage.java
@@ -7,19 +7,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class WebPage extends AppCompatActivity {
 
     private WebView webView;
+    private ImageButton shareButton;
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_page);
+
+        shareButton = (ImageButton)findViewById(R.id.share);
+        backButton = (ImageButton)findViewById(R.id.back);
+
 
         final ProgressDialog progressDialog = ProgressDialog.show(WebPage.this,"","Loading Article",true);
         webView = (WebView)findViewById(R.id.activity_main_webview);
@@ -29,7 +37,23 @@ public class WebPage extends AppCompatActivity {
 
         Intent mainPage = getIntent();
         Bundle extras = mainPage.getExtras();
-        String articleURL = extras.getString("url");
+        final String articleURL = extras.getString("url");
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "ABC News");
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, articleURL);
+                startActivity(Intent.createChooser(sharingIntent,"Share via"));
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
@@ -50,6 +74,11 @@ public class WebPage extends AppCompatActivity {
         webView.loadUrl(articleURL);
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 
     @Override
